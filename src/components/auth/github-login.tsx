d0358794d6d7173d4b2808927d0794208a469db7
Julien Constant <julienconstant190@gmail.com>
2024-03-23 00:04:16 +0100

    [fix] use default color for GitHub login btn

--- src/components/auth/github-login.tsx
+++ src/components/auth/github-login.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, useComputedColorScheme } from '@mantine/core';
+import { Button } from '@mantine/core';
 import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
@@ -10,12 +10,10 @@ import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 export const GitHubLogin = () => {
 	const searchParams = useSearchParams();
 	const callbackUrl = searchParams.get('callbackUrl');
-	const colorScheme = useComputedColorScheme();
 
 	return (
 		<Button 
 			variant="outline" 
-			color={colorScheme === 'dark' ? 'white' : 'black'}
 			leftSection={<FaGithub />}
 			onClick={() => {
 				signIn('github', {
