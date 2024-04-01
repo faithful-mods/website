f8b33acf4237c42458251ea649d67759f036b0ac
Julien Constant <julienconstant190@gmail.com>
2024-04-01 18:54:30 +0200

    feat: add tabs to dashboard

--- src/app/(pages)/(protected)/dashboard/page.tsx
+++ src/app/(pages)/(protected)/dashboard/page.tsx
@@ -1,28 +1,36 @@
 'use client';
 
-import { Grid, Stack } from '@mantine/core';
+import { Card, Tabs } from '@mantine/core';
 import { UserRole } from '@prisma/client';
 
 import { RoleGate } from '~/components/auth/role-gate';
 import { ModpacksPanel } from '~/components/dashboard/modpacks/modpacks-panel';
 import { ModsPanel } from '~/components/dashboard/mods/mods-panel';
 import { UsersPanel } from '~/components/dashboard/users/users-panel';
+import { gradient } from '~/lib/utils';
 
 const DashboardPage = () => {
 	return (
 		<RoleGate allowedRole={UserRole.ADMIN}>
-			<Grid gutter="sm" grow mt="sm">
-				<Grid.Col span={4}>
-					<UsersPanel />
-				</Grid.Col>
+			<Tabs defaultValue="1" variant="pills" color={gradient.to} >
+				<Card
+					shadow="sm" 
+					padding="md" 
+					radius="md"
+					mb="sm"
+					withBorder
+				>
+					<Tabs.List>
+						<Tabs.Tab value="1">Modpacks</Tabs.Tab>
+						<Tabs.Tab value="2">Mods</Tabs.Tab>
+						<Tabs.Tab value="3">Users</Tabs.Tab>
+					</Tabs.List>
+				</Card>
 
-				<Grid.Col span={8}>
-					<Stack gap="sm">
-						<ModpacksPanel />
-						<ModsPanel />
-					</Stack>
-				</Grid.Col>
-			</Grid>
+				<Tabs.Panel value="1"><ModpacksPanel /></Tabs.Panel>
+				<Tabs.Panel value="2"><ModsPanel /></Tabs.Panel>
+				<Tabs.Panel value="3"><UsersPanel /></Tabs.Panel>
+			</Tabs>
 		</RoleGate>
 	);
 };
