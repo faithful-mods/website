b9de42e27b3c19ffebb6c59a64eaa9dc95c6cacd
Julien Constant <julienconstant190@gmail.com>
2024-04-01 13:42:18 +0200

    tools: remove million lint (unused)

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import { next as MillionLint } from '@million/lint';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
 };
 
-const million = {
-	rsc: true
-}
-
-export default MillionLint(million)(nextConfig);
+export default nextConfig;
