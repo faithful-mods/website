86c5b8ed0cd10678d43944792c9e05e021c06ec8
Julien Constant <julienconstant190@gmail.com>
2024-04-14 23:47:09 +0200

    feat #20 : switch Select to a Menu component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import { ActionIcon, Badge, Button, Card, Combobox, Group, Image, useCombobox } from '@mantine/core';
+import { ActionIcon, Badge, Button, Card, Group, Image, Menu } from '@mantine/core';
 import { UserRole } from '@prisma/client';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import { FaHome } from 'react-icons/fa';
 import { GoLaw } from 'react-icons/go';
+import { GrGallery } from 'react-icons/gr';
 import { HiOutlineMenu } from 'react-icons/hi';
-import { IoMdSettings } from 'react-icons/io';
+import { IoMdCloudUpload, IoMdSettings } from 'react-icons/io';
 import { MdDashboard } from 'react-icons/md';
+import { TbPackage, TbPackages } from 'react-icons/tb';
 
 import { GitHubLogin } from '~/components/auth/github-login';
 import { LoggedUser } from '~/components/auth/logged-user';
@@ -20,23 +23,48 @@ import { ThemeSwitch } from './theme-switch';
 
 export const Navbar = () => {
 	const pathname = usePathname();
-	const router = useRouter();
 	const user = useCurrentUser();
-	const combobox = useCombobox();
 	const [windowWidth, _] = useDeviceSize();
 
 	const links = [
-		{ href: '/modpacks', label: 'Modpacks', disabled: false },
-		{ href: '/mods', label: 'Mods', disabled: false },
-		{ href: '/gallery', label: 'Gallery', disabled: false },
+		{
+			href: '/modpacks',
+			label: 'Modpacks',
+			disabled: false,
+			icon: <TbPackages />,
+		},
+		{
+			href: '/mods',
+			label: 'Mods',
+			disabled: false,
+			icon: <TbPackage />,
+		},
+		{
+			href: '/gallery',
+			label: 'Gallery',
+			disabled: false,
+			icon: <GrGallery />,
+		},
+
 	];
 
-	if (user) links.push({ href: '/contribute', label: 'Contribute', disabled: user.role === 'BANNED' });
-	if (windowWidth < BREAKPOINT_TABLET) links.push({ href: '/', label: 'Home', disabled: false });
+	if (user) links.push({
+		href: '/contribute',
+		label: 'Contribute',
+		disabled: user.role === 'BANNED',
+		icon: <IoMdCloudUpload />,
+	});
+
+	if (windowWidth < BREAKPOINT_TABLET) links.unshift({
+		href: '/',
+		label: 'Home',
+		disabled: false,
+		icon: <FaHome />,
+	});
 
 	return (
 		<Group gap="sm" mb="sm" mt="sm" wrap="nowrap">
-			{windowWidth >= BREAKPOINT_TABLET && 
+			{windowWidth >= BREAKPOINT_TABLET &&
 				<Card padding="sm" radius="md" withBorder shadow="sm" style={{ minWidth: '62px' }}>
 					<Link href="/" className="navbar-icon-fix">
 						<Image src="/icon.png" alt="FM" className="navbar-icon-fix" />
@@ -46,38 +74,42 @@ export const Navbar = () => {
 			<Card padding="sm" radius="md" withBorder className="w-full" shadow="sm">
 				<Group justify="space-between" wrap={windowWidth >= BREAKPOINT_TABLET ? 'wrap' : 'nowrap'}>
 					<Group gap="sm" wrap={windowWidth >= BREAKPOINT_TABLET ? 'wrap' : 'nowrap'}>
-						{windowWidth < BREAKPOINT_TABLET && 
-							<Combobox
-								store={combobox}
-								width={250}
+						{windowWidth < BREAKPOINT_TABLET &&
+							<Menu
+								shadow="md"
 								position="bottom-start"
-								withArrow
-								onOptionSubmit={(value) => {
-									router.push(links.find(link => link.label === value)?.href ?? '/');
-									combobox.closeDropdown();
-								}}
+								width={`calc(${windowWidth}px - (2 * var(--mantine-spacing-sm))`}
 							>
-								<Combobox.Target>
-									<Button 
-										onClick={() => combobox.toggleDropdown()}
+								<Menu.Target>
+									<Button
 										variant="transparent"
 										color={gradient.to}
 										className="navbar-icon-fix"
 									>
 										<HiOutlineMenu className="w-5 h-5" />
 									</Button>
-								</Combobox.Target>
+								</Menu.Target>
 
-								<Combobox.Dropdown>
-									<Combobox.Options>
-										{links.filter((l) => !l.disabled).map(link => link.label).sort().map((item) => (
-											<Combobox.Option value={item} key={item}>
-												{item}
-											</Combobox.Option>
-										))}
-									</Combobox.Options>
-								</Combobox.Dropdown>
-							</Combobox>
+								<Menu.Dropdown style={
+									{
+										left: 'var(--mantine-spacing-sm)',
+										top: 'calc(62px + (2 * var(--mantine-spacing-sm)))',
+									}
+								}>
+									{links.map((link, i) => (
+										<Menu.Item
+											key={i}
+											component="a"
+											href={link.href}
+											disabled={link.disabled}
+											leftSection={link.icon}
+											c={pathname === link.href ? gradient.to : undefined}
+										>
+											{link.label}
+										</Menu.Item>
+									))}
+								</Menu.Dropdown>
+							</Menu>
 						}
 						{user && user.role === UserRole.BANNED && windowWidth < BREAKPOINT_TABLET &&
 							<Badge color="red" variant="light">banned</Badge>
@@ -101,15 +133,15 @@ export const Navbar = () => {
 						{user && user.role === UserRole.BANNED && windowWidth >= BREAKPOINT_TABLET &&
 							<Badge color="red" variant="light">banned</Badge>
 						}
-						
+
 						<ThemeSwitch />
-						
+
 						{!user && <GitHubLogin />}
-						
-						{user && user.role === 'ADMIN' && 
+
+						{user && user.role === 'ADMIN' &&
 							<Link href='/dashboard'>
-								<ActionIcon 
-									size="lg" 
+								<ActionIcon
+									size="lg"
 									variant="transparent"
 									className="navbar-icon-fix"
 								><MdDashboard className="w-5 h-5"/></ActionIcon>
@@ -118,18 +150,18 @@ export const Navbar = () => {
 
 						{user && user.role === UserRole.COUNCIL &&
 							<Link href='/council'>
-								<ActionIcon 
-									size="lg" 
+								<ActionIcon
+									size="lg"
 									variant="transparent"
 									className="navbar-icon-fix"
 								><GoLaw className="w-5 h-5"/></ActionIcon>
 							</Link>
 						}
-						
+
 						{user && (user.role !== UserRole.BANNED || windowWidth !== BREAKPOINT_TABLET) &&
 							<Link href='/settings/me'>
-								<ActionIcon 
-									size="lg" 
+								<ActionIcon
+									size="lg"
 									variant="transparent"
 									disabled={user.role === UserRole.BANNED}
 									className={user.role === UserRole.BANNED ? 'navbar-icon-fix button-disabled' : 'navbar-icon-fix'}
