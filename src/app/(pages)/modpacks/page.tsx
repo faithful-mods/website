c93947f9fd192c937825ea09ecc211a50b49c025
Julien Constant <julienconstant190@gmail.com>
2024-08-07 23:38:01 +0200

    feat : improve temp pages

--- src/app/(pages)/modpacks/page.tsx
+++ src/app/(pages)/modpacks/page.tsx
@@ -1,16 +1,9 @@
-import { Poppins } from 'next/font/google';
-
-import { cn } from '~/lib/utils';
-
-const font = Poppins({
-	subsets: ['latin'],
-	weight: ['600'],
-});
+import { Group, Text } from '@mantine/core';
 
 export default async function Modpacks() {
 	return (
-		<main className="flex flex-col items-center justify-center">
-			<h1 className={cn(font)}>Modpacks page</h1>
-		</main>
+		<Group align="center" className="w-full" h="500">
+			<Text fw={300} size="xl" className="w-full" ta="center">🚧 Page under construction 🚧</Text>
+		</Group>
 	);
 }
