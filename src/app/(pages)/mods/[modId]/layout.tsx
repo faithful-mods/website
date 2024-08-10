891c3659d0eb96be1274580066959fcdad6f46e0
Julien Constant <julienconstant190@gmail.com>
2024-08-10 14:39:28 +0200

    feat : rework loading & mod not found pages

--- src/app/(pages)/mods/[modId]/layout.tsx
+++ src/app/(pages)/mods/[modId]/layout.tsx
@@ -6,13 +6,13 @@ import { useState } from 'react';
 
 import { HiDownload } from 'react-icons/hi';
 import { IoExtensionPuzzleOutline } from 'react-icons/io5';
+import { IoChevronBackOutline } from 'react-icons/io5';
 import { TfiWorld } from 'react-icons/tfi';
 
-import { Button, Group, Stack, Text } from '@mantine/core';
+import { Button, Group, Stack, Text, Title } from '@mantine/core';
 
 import { TabsLayout } from '~/components/tabs';
 import { TextureImage } from '~/components/texture-img';
-import { Tile } from '~/components/tile';
 import { useDeviceSize } from '~/hooks/use-device-size';
 import { useEffectOnce } from '~/hooks/use-effect-once';
 import { BREAKPOINT_MOBILE_LARGE } from '~/lib/constants';
@@ -41,106 +41,115 @@ export default function ModLayout({ children }: { children: React.ReactNode }) {
 			.finally(() => setLoading(false));
 	});
 
+	if (!mod && isLoading) {
+		return (
+			<Stack
+				align="center"
+				justify="center"
+				gap="md"
+				style={{ height: 'calc(81% - (2 * var(--mantine-spacing-sm) - 62px))' }}
+			>
+				<Title><Text component="span" fw={300} inherit>Loading</Text></Title>
+				<Text size="lg">Fetching mod data...</Text>
+			</Stack>
+		);
+	}
+
+	if (!mod) {
+		return (
+			<Stack
+				align="center"
+				justify="center"
+				gap="md"
+				style={{ height: 'calc(81% - (2 * var(--mantine-spacing-sm) - 62px))' }}
+			>
+				<Title>404&nbsp;<Text component="span" fw={300} inherit>Mod Not Found</Text></Title>
+				<Text size="lg">The mod you are looking for does not exist</Text>
+				<Button
+					component="a"
+					href="/mods"
+					variant="link"
+					mt="md"
+					leftSection={<IoChevronBackOutline />}
+				>
+					Back to mods
+				</Button>
+			</Stack>
+		);
+	}
+
 	return (
 		<Stack gap="sm" mb="sm">
-			{!mod && isLoading && (
-				<Tile>
-					<Group align="center" justify="center" style={{ height: '120px' }}>
-						<Text size="xl">Loading...</Text>
-					</Group>
-				</Tile>
-			)}
-			{!mod && !isLoading && (
-				<Tile>
-					<Stack align="center" justify="center" style={{ height: '120px' }}>
-						<Text size="xl">
-							Mod not found
-						</Text>
-						<Button
-							component="a"
-							href="/mods"
-							variant="link"
-						>
-							Back to mods
-						</Button>
-					</Stack>
-				</Tile>
-			)}
-
-			{mod && (
-				<>
-					<Stack gap="xs">
-						<Group wrap="nowrap">
-							<TextureImage
-								src={mod.image ?? './icon.png'}
-								alt={mod.name}
-								size={windowWidth <= BREAKPOINT_MOBILE_LARGE ? '85px' : '120px'}
-								styles={{
-									borderRadius: 5,
-								}}
-							/>
-							<Stack
-								align="start"
-								gap="xs"
-								h={windowWidth <= BREAKPOINT_MOBILE_LARGE ? 85 : 120}
-								w="100%"
-							>
-								<Stack gap={0}>
-									<Group gap={5} align="baseline">
-										<Text fw={700} size="md">{mod.name}</Text>
-										{mod.authors.length > 0 && (
-											<Text size="xs" c="dimmed">
+			<Stack gap="xs">
+				<Group wrap="nowrap">
+					<TextureImage
+						src={mod.image ?? './icon.png'}
+						alt={mod.name}
+						size={windowWidth <= BREAKPOINT_MOBILE_LARGE ? '85px' : '120px'}
+						styles={{
+							borderRadius: 5,
+						}}
+					/>
+					<Stack
+						align="start"
+						gap="xs"
+						h={windowWidth <= BREAKPOINT_MOBILE_LARGE ? 85 : 120}
+						w="100%"
+					>
+						<Stack gap={0}>
+							<Group gap={5} align="baseline">
+								<Text fw={700} size="md">{mod.name}</Text>
+								{mod.authors.length > 0 && (
+									<Text size="xs" c="dimmed">
 													by {mod.authors.slice(0, slice).join(', ')}
-												{mod.authors.length > (slice - 1) && ` and ${mod.authors.length - (slice - 1)} more...`}
-											</Text>
-										)}
-									</Group>
-									{mod.description && (<Text size="sm" lineClamp={windowWidth <= BREAKPOINT_MOBILE_LARGE ? 1 : 2}>{mod.description}</Text>)}
-									{!mod.description && (<Text size="sm" c="dimmed">No description</Text>)}
-								</Stack>
+										{mod.authors.length > (slice - 1) && ` and ${mod.authors.length - (slice - 1)} more...`}
+									</Text>
+								)}
+							</Group>
+							{mod.description && (<Text size="sm" lineClamp={windowWidth <= BREAKPOINT_MOBILE_LARGE ? 1 : 2}>{mod.description}</Text>)}
+							{!mod.description && (<Text size="sm" c="dimmed">No description</Text>)}
+						</Stack>
 
-								<Group gap="xl" align="end" justify="center" mt={-4}>
-									{mod.url && (
-										<Button
-											component="a"
-											href={mod.url}
-											variant="transparent"
-											leftSection={<TfiWorld />}
-											p={0}
-										>
+						<Group gap="xl" align="end" justify="center" mt={-4}>
+							{mod.url && (
+								<Button
+									component="a"
+									href={mod.url}
+									variant="transparent"
+									leftSection={<TfiWorld />}
+									p={0}
+								>
 											Website
-										</Button>
-									)}
-									<Group gap="xs" wrap="nowrap" align="center" style={{ height: '36px' }}>
-										<IoExtensionPuzzleOutline color="var(--mantine-color-dimmed)" />
-										<Text size="sm" c="dimmed">
-											{mod.loaders.join(', ')}
-										</Text>
-									</Group>
-									<Group gap="xs" wrap="nowrap" align="center" style={{ height: '36px' }}>
-										<HiDownload color="var(--mantine-color-dimmed)" />
-										<Text size="sm" c="dimmed">
-											{downloads ? Object.values(downloads).reduce<number>((acc, curr) => acc + (curr ?? 0), 0) : 0}
-										</Text>
-									</Group>
-								</Group>
-							</Stack>
+								</Button>
+							)}
+							<Group gap="xs" wrap="nowrap" align="center" style={{ height: '36px' }}>
+								<IoExtensionPuzzleOutline color="var(--mantine-color-dimmed)" />
+								<Text size="sm" c="dimmed">
+									{mod.loaders.join(', ')}
+								</Text>
+							</Group>
+							<Group gap="xs" wrap="nowrap" align="center" style={{ height: '36px' }}>
+								<HiDownload color="var(--mantine-color-dimmed)" />
+								<Text size="sm" c="dimmed">
+									{downloads ? Object.values(downloads).reduce<number>((acc, curr) => acc + (curr ?? 0), 0) : 0}
+								</Text>
+							</Group>
 						</Group>
 					</Stack>
+				</Group>
+			</Stack>
 
-					<TabsLayout
-						defaultValue="downloads"
-						isLayout
-						variant="filled"
-						tabs={[
-							{ value: 'downloads', label: 'Downloads', layoutTab: true },
-							{ value: 'gallery', label: 'Gallery' },
-						]}
-					>
-						{children}
-					</TabsLayout>
-				</>
-			)}
+			<TabsLayout
+				defaultValue="downloads"
+				isLayout
+				variant="filled"
+				tabs={[
+					{ value: 'downloads', label: 'Downloads', layoutTab: true },
+					{ value: 'gallery', label: 'Gallery' },
+				]}
+			>
+				{children}
+			</TabsLayout>
 		</Stack>
 	);
 }
