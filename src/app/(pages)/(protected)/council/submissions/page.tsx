678506fa429a3a5875d3a4f75f6d1a24961664fa
Julien Constant <julienconstant190@gmail.com>
2024-08-12 01:26:22 +0200

    fix : rework submissions UX

--- src/app/(pages)/(protected)/council/submissions/page.tsx
+++ src/app/(pages)/(protected)/council/submissions/page.tsx
@@ -2,10 +2,10 @@
 
 import { useState } from 'react';
 
-import { GoInfo } from 'react-icons/go';
+import { GoAlert, GoCheckCircle, GoCircleSlash } from 'react-icons/go';
 
-import { Badge, Group, Stack, Switch, Text } from '@mantine/core';
-import { UserRole } from '@prisma/client';
+import { Accordion, Badge, Group, Select, Stack, Switch, Text } from '@mantine/core';
+import { Resolution, UserRole } from '@prisma/client';
 
 import { Tile } from '~/components/tile';
 import { useCurrentUser } from '~/hooks/use-current-user';
@@ -21,14 +21,19 @@ import type { ContributionWithCoAuthorsAndFullPoll, PublicUser } from '~/types';
 
 const CouncilContributionsPanel = () => {
 	const [textures, setTextures] = useState<Texture[]>([]);
+
 	const [isLightBackground, setLightBackground] = useState(false);
 	const [hasBorder, setBorder] = useState(false);
+	const [showVoted, setShowVoted] = useState(false);
 
 	const [counselors, setCounselors] = useState<PublicUser[]>([]);
 	const [counselorVoted, setCounselorVoted] = useState<ContributionWithCoAuthorsAndFullPoll[]>([]);
 	const [counselorUnvoted, setCounselorUnvoted] = useState<ContributionWithCoAuthorsAndFullPoll[]>([]);
 
 	const counselor = useCurrentUser()!;
+
+	const infoType = counselor.role === UserRole.ADMIN ? 'red' : counselorUnvoted.length > 0 ? 'yellow' : 'teal';
+	const infoTypeText = counselor.role === UserRole.ADMIN ? 'var(--text-color)' : counselorUnvoted.length > 0 ? 'black' : 'white';
 
 	useEffectOnce(() => {
 		getTextures()
@@ -63,98 +68,125 @@ const CouncilContributionsPanel = () => {
 
 	return (
 		<Stack gap="sm" mb="sm">
-			<Tile>
-				<Text size="md" fw={700}>Submission Process</Text>
-				<Text size="sm" c="dimmed">
-					Contributors submissions are reviewed here by the council. Seing this page means you are part of the council (congrats!)
-				</Text>
-				<Text size="sm">
-					All contributions submitted by contributors will be reviewed by all council members. Each council member will have to vote for each contribution.
-					Once all council members have voted, the contribution will be accepted or rejected following these rules:
-				</Text>
-				<ul>
-					<Text component="li" size="sm">
-						If the contribution has more upvotes than downvotes, it will be <Badge component="span" color="teal">accepted</Badge>
-					</Text>
-					<Text component="li" size="sm">
-						If there is more downvotes or the same amount of upvotes and downvotes, it will be <Badge component="span" color="red">rejected</Badge>
-					</Text>
-					<Text component="li" c="red" size="sm">
-						Once the voted is ended, the contribution status will be definitive.
-					</Text>
-				</ul>
-			</Tile>
-			<Tile color={counselor.role === UserRole.ADMIN ? 'red' : counselorUnvoted.length > 0 ? 'yellow': 'teal'}>
-				<Group wrap="nowrap">
-					<GoInfo size={20} color={counselor.role === UserRole.ADMIN ? 'var(--text-color)' : counselorUnvoted.length > 0 ? 'black' : 'white'} />
-					{counselor.role === UserRole.COUNCIL && (
-						<Text size="sm" c={counselorUnvoted.length > 0 ? 'black' : 'white'}>
-							{counselorUnvoted.length === 0 && counselorVoted.length === 0 && (
-								'No more contributions in the voting process, nice!'
-							)}
-							{counselorUnvoted.length > 0 && (
-								`There ${counselorUnvoted.length === 1 ? 'is' : 'are'} ${counselorUnvoted.length} contribution${counselorUnvoted.length > 1 ? 's' : ''} waiting for your vote.`
-							)}
+
+			<Accordion chevronPosition="right">
+				<Accordion.Item value="about">
+					<Accordion.Control>
+						<Text size="md" fw={700}>
+							About the submissions review process
 						</Text>
-					)}
-					{counselor.role === UserRole.ADMIN && (
+					</Accordion.Control>
+					<Accordion.Panel>
+						<Text size="sm" c="dimmed">
+							Contributors submissions are reviewed here by the council. Seing this page means you are part of the council (congrats!)
+						</Text>
 						<Text size="sm">
-							You&apos;re an admin, you can&apos;t vote on contributions.
+							All contributions submitted by contributors will be reviewed by all council members. Each council member will have to vote for each contribution.
+							Once all council members have voted, the contribution will be accepted or rejected following these rules:
 						</Text>
+						<ul>
+							<Text component="li" size="sm">
+								If the contribution has more upvotes than downvotes, it will be <Badge component="span" color="teal">accepted</Badge>
+							</Text>
+							<Text component="li" size="sm">
+								If there is more downvotes or the same amount of upvotes and downvotes, it will be <Badge component="span" color="red">rejected</Badge>
+							</Text>
+							<Text component="li" c="red" size="sm">
+								Once the voted is ended, the contribution status will be definitive.
+							</Text>
+						</ul>
+					</Accordion.Panel>
+				</Accordion.Item>
+			</Accordion>
+
+			<Tile color={infoType}>
+				<Group gap="xs" wrap="nowrap">
+					{infoType === 'red' && (
+						<>
+							<GoCircleSlash color={infoTypeText} />
+							<Text size="sm" c={infoTypeText}>
+								You are an admin, you can&apos;t vote on contributions.
+							</Text>
+						</>
+					)}
+
+					{infoType === 'yellow' && (
+						<>
+							<GoAlert color={infoTypeText} />
+							<Text size="sm" c={infoTypeText}>
+								There {counselorUnvoted.length === 1 ? 'is' : 'are'} {counselorUnvoted.length} contribution{counselorUnvoted.length > 1 ? 's' : ''} waiting for your vote.
+							</Text>
+						</>
+					)}
+
+					{infoType === 'teal' && (
+						<>
+							<GoCheckCircle size={20} color={infoTypeText} />
+							<Text size="sm" c={infoTypeText}>
+								No more contributions to vote on. Good job!
+							</Text>
+						</>
 					)}
 				</Group>
 			</Tile>
 
-			<Tile>
-				<Group gap="sm">
-					<Switch
-						label="Light background"
-						checked={isLightBackground}
-						onChange={() => setLightBackground(!isLightBackground)}
-					/>
-					<Switch
-						label="Add border"
-						checked={hasBorder}
-						onChange={() => setBorder(!hasBorder)}
+			<Tile p="xs" pl="md">
+				<Group justify="space-between">
+					<Group gap="sm">
+						<Switch
+							label="Show voted contributions"
+							checked={showVoted}
+							onChange={(e) => setShowVoted(e.target.checked)}
+						/>
+						<Switch
+							label="Light background"
+							checked={isLightBackground}
+							onChange={(e) => setLightBackground(e.target.checked)}
+						/>
+						<Switch
+							label="Add border"
+							checked={hasBorder}
+							onChange={(e) => setBorder(e.target.checked)}
+						/>
+					</Group>
+					<Select
+						placeholder="All resolutions"
+						data={Object.keys(Resolution).map((r) => ({ value: r, label: r }))}
+						checkIconPosition="right"
+						clearable
+						w={140}
 					/>
 				</Group>
 			</Tile>
 
-			<Group gap="sm">
-				{counselorUnvoted.map((c, i) =>
-					<CouncilContributionItem
-						disabled={counselor.role === UserRole.ADMIN}
-						key={i}
-						counselors={counselors}
-						contribution={c}
-						texture={textures.find(t => t.id === c.textureId)!}
-						onVote={loadContributions}
-						isLightBackground={isLightBackground}
-						hasBorder={hasBorder}
-					/>
-				)}
-			</Group>
-			{counselorVoted.length > 0 && (
-				<Tile>
-					<Text>
-						Contributions that you have already voted on and that are waiting for other council members to vote on:
-					</Text>
-				</Tile>
+			{(counselorVoted.length > 0 || infoType !== 'teal') && (
+				<Group gap="sm">
+					{counselorUnvoted.map((c, i) =>
+						<CouncilContributionItem
+							disabled={counselor.role === UserRole.ADMIN}
+							key={i}
+							counselors={counselors}
+							contribution={c}
+							texture={textures.find(t => t.id === c.textureId)!}
+							onVote={loadContributions}
+							isLightBackground={isLightBackground}
+							hasBorder={hasBorder}
+						/>
+					)}
+					{showVoted && counselorVoted.map((c, i) =>
+						<CouncilContributionItem
+							disabled={counselor.role === UserRole.ADMIN}
+							key={i}
+							counselors={counselors}
+							contribution={c}
+							texture={textures.find(t => t.id === c.textureId)!}
+							onVote={loadContributions}
+							isLightBackground={isLightBackground}
+							hasBorder={hasBorder}
+						/>
+					)}
+				</Group>
 			)}
-			<Group gap="sm">
-				{counselorVoted.map((c, i) =>
-					<CouncilContributionItem
-						disabled={counselor.role === UserRole.ADMIN}
-						key={i}
-						counselors={counselors}
-						contribution={c}
-						texture={textures.find(t => t.id === c.textureId)!}
-						onVote={loadContributions}
-						isLightBackground={isLightBackground}
-						hasBorder={hasBorder}
-					/>
-				)}
-			</Group>
 		</Stack>
 	);
 };
