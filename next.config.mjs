3c5c4e64224e35a0a768a1320e178bcae7a5f756
Julien Constant <julienconstant190@gmail.com>
2025-04-22 23:43:05 +0200

    fix : manual fix while waiting for ZJONSSON/node-unzipper#330

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	webpack: (config, { webpack }) => {
+		// Manual fix for https://github.com/ZJONSSON/node-unzipper/issues/330
+		return {
+			...config,
+			plugins: [
+				...config.plugins,
+				new webpack.IgnorePlugin({ resourceRegExp: /^@aws-sdk\/client-s3$/ }),
+			],
+		}
+	}
 };
 
 export default nextConfig;
